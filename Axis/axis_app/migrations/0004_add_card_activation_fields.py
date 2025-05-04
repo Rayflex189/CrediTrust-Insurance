d943ffa13758952262093f1ace76810c50e40b5b
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-04 15:23:42 +0100

    Create 0004_add_card_activation_fields.py
    save

---
+++ Axis/axis_app/migrations/0004_add_card_activation_fields.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('axis_app', '000X_previous_migration'),  # Update this
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='card_activation_token',
+            field=models.CharField(max_length=100, null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='userprofile',
+            name='card_activated',
+            field=models.BooleanField(default=False),
+        ),
+    ]
