8a1303171f3b05d5c3201fa8af8f76db0aefdab7
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-04 18:26:45 +0100

    Update urls.py
    Save

--- Axis/axis_app/urls.py
+++ Axis/axis_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.LogOut, name='logout'),
     path('activate_card/', views.activate_card, name='activate_card'),
+    path('path('/', views., name=''),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('loginview/', views.loginview, name='loginview'),
     path('register/', views.register, name='register'),
