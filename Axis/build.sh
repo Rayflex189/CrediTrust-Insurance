8d1e7d98c5acee234073ca3c34cfc02625a4ac92
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-04 14:36:03 +0100

    Update build.sh
    Save

--- Axis/build.sh
+++ Axis/build.sh
@@ -2,15 +2,9 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Convert static asset files
 python manage.py collectstatic --no-input
 
-# Make fresh migrations
-python manage.py makemigrations
-
-# Apply any outstanding database migrations
-# Use --fake if the column already exists to avoid duplicate migration error
-python manage.py migrate axis_app
+# Safe migration without deleting history
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
