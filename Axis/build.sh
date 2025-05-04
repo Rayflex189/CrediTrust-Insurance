59667085aaa4dcaad4212152cdc630d1fc6da073
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-04 15:33:31 +0100

    Update build.sh
    Save

--- Axis/build.sh
+++ Axis/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Reset migrations safely
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
-# Make and apply fresh migrations
-python manage.py makemigrations --noinput
-python manage.py migrate --fake-initial --noinput
+# Run only migration (not makemigrations)
+python manage.py migrate
