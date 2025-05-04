50783a2a2430a2643fe01ba8088f6fa0b69a6b1c
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-04 06:32:18 +0100

    Update signals.py
    Save

--- Axis/axis_app/signals.py
+++ Axis/axis_app/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import UserProfile, Transaction
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=UserProfile)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         profile = UserProfile.objects.create(user=instance)
