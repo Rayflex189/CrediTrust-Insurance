4af5ee72ef443a55ab9180945cfdc8f0b8fad8b0
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-23 06:51:12 +0100

    Update settings.py
    Save

--- Axis/Axis/settings.py
+++ Axis/Axis/settings.py
@@ -15,14 +15,16 @@ import os
 
 # settings.py
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
