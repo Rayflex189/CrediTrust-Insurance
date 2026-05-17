55ff81ff937028f2e2846f478d9ecc81c2285bc6
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-05-17 08:03:31 +0100

    Update entrypoint.sh
    Save

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,17 @@ set -o errexit
 echo "Running database migrations..."
 python manage.py migrate --no-input
 
+echo "Creating superuser if it doesn't exist..."
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='Paul').exists():
+    User.objects.create_superuser('Paul', 'admin@example.com', 'Me12sleep');
+    print('Superuser "Paul" created successfully!');
+else:
+    print('Superuser "Paul" already exists');
+EOF
+
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
